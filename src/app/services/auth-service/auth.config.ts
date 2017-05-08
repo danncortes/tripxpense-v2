df7f422f04662f6504322d2d38d361517e6e5927
Daniel Cortes <danncortes@gmail.com>
2017-05-07 22:25:43 -0500

    fixing auth

--- src/app/services/auth-service/auth.config.ts
+++ src/app/services/auth-service/auth.config.ts
@@ -8,6 +8,11 @@ export const authConfig: AuthConfiguration = {
     clientID: 'vr58-dvu3gMKrBJw_nzExEJdv3sirBPi',
     domain: 'tripxpense.auth0.com',
     options: {
-        allowedConnections: ['Username-Password-Authentication', 'facebook', 'google-oauth2']
+        allowedConnections: ['Username-Password-Authentication', 'facebook', 'google-oauth2'],
+        auth: {
+            redirectUrl: location.origin,
+            responseType: 'token'
+        }
     }
+    
 };

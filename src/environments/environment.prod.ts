bf1f11e8ab6ca4f15bc5d432178814c810f00162
Daniel Cortes <danncortes@gmail.com>
2017-06-28 07:03:50 -0500

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  apiUrl : 'http://proyectos.local:8888/tripxpense-api/public'
+  apiUrl : 'http://api.tripxpense.com/public'
 };

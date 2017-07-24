cfb056dc0a5444f0334ed076348bba05ee133891
Daniel Cortes <danncortes@gmail.com>
2017-07-23 21:35:47 -0500

    dist

---
+++ dist/assets/charts.config.ts
@@ -0,0 +1,7 @@
+export const chartsConfig = {
+  colors : {
+      spent : ['#e65c00', '#b34700', '#803300'],
+      operations : ['#0052cc', '#003d99', '#002966'],
+      income : ['#2eb82e', '#248f24', '#196619']
+  }
+};

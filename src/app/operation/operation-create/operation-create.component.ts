d37e54ad665843014898e5a1c1e8dc0037867d27
Daniel Cortes <danncortes@gmail.com>
2017-10-19 07:31:45 -0500

    Keep building create operation form

--- src/app/operation/operation-create/operation-create.component.ts
+++ src/app/operation/operation-create/operation-create.component.ts
@@ -1,8 +1,12 @@
+import { PaymethodService } from './../../services/paymethod/paymethod.service';
+import { CategoryService } from './../../services/category/category.service';
+import { TravelService } from './../../services/travel/travel.service';
 import { Component, OnInit } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ToastService } from '../../services/toast/toast.service';
 import { OperationService } from '../../services/operation/operation.service';
+import { AuthService } from '../../services/auth-service/auth.service';
 
 @Component({
     selector: 'app-operation-create',
@@ -10,30 +14,72 @@ import { OperationService } from '../../services/operation/operation.service';
     styleUrls: ['./operation-create.component.scss'],
     providers: [
         ToastService,
-        OperationService
+        OperationService,
+        TravelService,
+        CategoryService,
+        PaymethodService
     ]
 })
 export class OperationCreateComponent implements OnInit {
 
     createOperationForm: FormGroup;
     processing: boolean;
+    categories: any;
+    travels: any
+    payMethods: any;
 
     constructor(
         public dialogRef: MdDialogRef<OperationCreateComponent>,
         formBuilder: FormBuilder,
         private OperationService: OperationService,
-        public toastService: ToastService
+        public toastService: ToastService,
+        private travelService: TravelService,
+        private categoryService: CategoryService,
+        private payMethodService: PaymethodService,
+        private auth: AuthService
     ) {
         this.createOperationForm = formBuilder.group({
             'title': [null, Validators.required],
             'description': [null],
             'cost': [null, Validators.required],
             'date_op': [null, Validators.required],
-            'type': [1, Validators.required]
+            'type': ['outcome', Validators.required],
+            'cod_method': [null, Validators.required],
+            'cod_category': [null, Validators.required],
+            'cod_travel': [null, Validators.required]
         });
     }
 
     ngOnInit() {
+        this.getTravels();
+        this.getCategories();
+        this.getPayMethods();
+    }
+
+    getTravels = () => {
+        const userId = this.auth.getUserId();
+        this.travelService.get(userId).subscribe(
+            data => {
+                this.travels = data;
+            }
+        );
+    }
+
+    getPayMethods = () => {
+        this.payMethodService.get().subscribe(
+            data => {
+                this.payMethods = data;
+                this.processing = false;
+            }
+        );
+    }
+
+    getCategories = () => {
+        this.categoryService.get().subscribe(
+            data => {
+                this.categories = data;
+            }
+        )
     }
 
     createOperation(formData) {
